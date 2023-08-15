02d2162437329ebc5858dc19705236bf4dcb7306
Amith Kumar Manjunath <ammanjunath@confluent.io>
2023-08-15 18:32:36 -0400

    updates

---
+++ connect/oracle/connect-cdc-oracle11/c3-ui/something.js
@@ -0,0 +1,10 @@
+document.onload = document.getElementById("controlcenter").innerHTML =
+                "Welcome to Custom dynamic Message" ;
+
+
+  // function EnterName() {
+  //           let x = document.getElementById("name").value;
+ 
+  //           document.getElementById("controlcenter").innerHTML =
+  //               "Welcome to Geeks For Geeks " + x;
+  //       }
